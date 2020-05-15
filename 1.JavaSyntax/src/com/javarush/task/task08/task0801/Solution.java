8f9f9668a3a72dc12a110e6a6655c18b1447bc4d
Anton Seliverstov <antoncadetsil@gmail.com>
2020-05-15 20:52:42 +0100

    Adding JavaRush tasks completion

--- 1.JavaSyntax/src/com/javarush/task/task08/task0801/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0801/Solution.java
@@ -4,9 +4,31 @@ package com.javarush.task.task08.task0801;
 Set из растений
 */
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+
+        HashSet<String> stringSet = new HashSet<>();
+        Iterator<String> iterator = stringSet.iterator();
+        stringSet.add("арбуз");
+        stringSet.add("банан");
+        stringSet.add("вишня");
+        stringSet.add("груша");
+        stringSet.add("дыня");
+        stringSet.add("ежевика");
+        stringSet.add("женьшень");
+        stringSet.add("земляника");
+        stringSet.add("ирис");
+        stringSet.add("картофель");
+
+        for (String text : stringSet) {
+            System.out.println(text);
+        }
+
+
+
 
     }
 }
---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0802/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task08.task0802;
+
+/* 
+Map из 10 пар
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> stringMap = new HashMap<>();
+
+        stringMap.put("арбуз", "ягода");
+        stringMap.put("банан", "трава");
+        stringMap.put("вишня", "ягода");
+        stringMap.put("груша", "фрукт");
+        stringMap.put("дыня", "овощ");
+        stringMap.put("ежевика", "куст");
+        stringMap.put("жень-шень", "корень");
+        stringMap.put("земляника", "ягода");
+        stringMap.put("ирис", "цветок");
+        stringMap.put("картофель", "клубень");
+
+        for (Map.Entry<String, String> pair : stringMap.entrySet()) {
+            String key = pair.getKey();
+            String data = pair.getValue();
+            System.out.println(key + " - " + data);
+        }
+
+    }
+}
